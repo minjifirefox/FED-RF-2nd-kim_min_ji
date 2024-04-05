a329db8bd39b692a3688108876e653bd70226f81
zoe <alswl3125@naver.com>
2024-04-05 16:21:51 +0900

    2024.04.05

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/slide.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/slide.js
@@ -120,7 +120,7 @@ export default function slideFn() {
       // 함수를 호출시에 아무값도 보내지 않으면
       // 함수의 전달변수 하나를 쓸 경우 또는
       // 여러 전달 변수 중 첫번째 변수는 이벤트 객체가 전달된다!
-      console.log("전달변수:", evt, sts);
+      // console.log("전달변수:", evt, sts);
 
       // 만약 버튼 클릭일 경우 인터발 지우기 함수 호출!
       if (sts) {
@@ -148,7 +148,7 @@ export default function slideFn() {
       // 해당 클래스가 있으면 true,없으면 false를 리턴한다
 
       // 함수 호출 확인
-      console.log("나 슬라이드야~!", this, isRbtn);
+      // console.log("나 슬라이드야~!", this, isRbtn);
       // this는 호출한 버튼 자신
 
       // 2. 버튼별 분기하기 //////
@@ -210,7 +210,7 @@ export default function slideFn() {
       let seq = slide
         .querySelectorAll("li")
         [isRbtn ? 1 : 0].getAttribute("data-seq");
-      console.log("블릿이 읽어올 슬라이드 순번:", seq, "데이터형:", typeof seq);
+      // console.log("블릿이 읽어올 슬라이드 순번:", seq, "데이터형:", typeof seq);
       // string - 문자형, number - 숫자형
 
       // 4. 블릿 변경하기
