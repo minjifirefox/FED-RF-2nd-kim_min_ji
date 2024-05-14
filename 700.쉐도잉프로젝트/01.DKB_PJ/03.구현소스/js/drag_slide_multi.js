7919bceb8cee5f99711182b06097309d959f55cc
zoe <alswl3125@naver.com>
2024-05-14 18:17:55 +0900

    2024.05.14

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/drag_slide_multi.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/drag_slide_multi.js
@@ -60,7 +60,7 @@ function slideFn(selEl) {
   // let indic = mFn.qsEl(sldWrap, ".indic");
 
   // 대상확인
-  console.log("대상:", abtn);
+  // console.log("대상:", abtn);
 
   // 1.4. 슬라이드 개수와 동일한 블릿동적생성
   // 대상: .indic -> indic변수
@@ -371,7 +371,7 @@ function slideFn(selEl) {
       // 전체 슬라이드 width = li한개당width * 슬라이드개수
       let limitSize = 
       selEl.offsetWidth - (oneSize * listLength);
-      console.log('마지막한계left:',limitSize);
+      // console.log('마지막한계left:',limitSize);
 
       // 4-1. 맨앞에서 튕기기 ////////////
       if(parseInt(dtg.style.left)>0){
@@ -483,11 +483,11 @@ function slideFn(selEl) {
     // (3) 한개li크기로 현재 left위치크기를 나누어서
     // 소수점 아래결과는 반올림해준다! -> 특정위치로 이동함!
     let divideNum = parseInt(dtg.style.left) / oneSize;
-    console.log('나눈수:',divideNum);
+    // console.log('나눈수:',divideNum);
     divideNum = Math.round(divideNum);
-    console.log('나눈수 반올림:',divideNum);
+    // console.log('나눈수 반올림:',divideNum);
     divideNum = Math.abs(divideNum);
-    console.log('나눈수 반올림후 절대값:',divideNum);
+    // console.log('나눈수 반올림후 절대값:',divideNum);
 
     // 특정위치로 이동하기 : 한개당크기 * 개수
     dtg.style.left = -(oneSize * divideNum) + "px";
--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -89,26 +89,36 @@ introMv.onclick = () => {
 
   // 1. 8개만 데이터를 html로 구성하여 넣는다!
   // html 코드변수
-  let hcode = `<ul class="fx-box">`;
+  // let hcode = `<ul class="fx-box">`;
 
   // li구성을 hcode변수에 대입연산자로 할당함!
-  for (let i = 0; i < 8; i++) {
-    hcode += `
-        <li>
-            <h3>${pData[i].title}</h3>
-            <p>${pData[i].story}</p>
-        </li>
-    `;
-  } //// for //////
+  // for (let i = 0; i < 8; i++) {
+  //   hcode += `
+  //       <li>
+  //           <h3>${pData[i].title}</h3>
+  //           <p>${pData[i].story}</p>
+  //       </li>
+  //   `;
+  // } //// for //////
 
-  hcode += `</ul>`;
+  // hcode += `</ul>`;
 
   // 데이터 확인
   // console.log(hcode);
   // console.log('대상:',previewBox,'미리보기 data:',pData);
 
-  // 2. 화면출력하기 ///////
-  previewBox.innerHTML = hcode;
+  // 2. 화면출력하기 - map()으로 한 번에 출력하자! ///////
+  previewBox.innerHTML = `
+    <ul class="fx-box">
+      ${pData.map(v=>`
+        <li data-idx="${v.idx}">
+          <h3>${v.title}</h3>
+          <p>${"방송일 : "+v.date+" "+v.story}</p>
+        </li>
+      `).join('')}
+      
+    </ul>
+  `;
 })(); //// 미리보기 코드랩핑구역 종료 /////////
 
 // 3. 현장포토 파트 내용 넣기 //////////
