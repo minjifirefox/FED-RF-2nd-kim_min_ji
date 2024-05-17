2bccea88fc527e874fae9aa24a244a61fe9f4258
zoe <alswl3125@naver.com>
2024-05-17 18:16:22 +0900

    2024.05.17

--- 004.React학습/02.react-ex/js/데이터생성.js
+++ 004.React학습/02.react-ex/js/데이터생성.js
@@ -1,23 +1,24 @@
 
-// 공유 신발 데이터 불러오기
-import guData from "./data/gu_data.js";
+// 공유신발 데이터 불러오기
+import guData from "./data/gu_data_old.js";
 
 console.log(guData);
 
-// 국가 옵션 배열
-const country = ["캄보디아","베트남","중국","러시아","일본"];
+// 국가 옵션배열
+const country = 
+["캄보디아","베트남","중국","러시아","일본"];
 const countryNum = country.length;
 
-// 랜덤수 생성 함수
+// 랜덤수 생성함수
 const rdmNum = (n) => Math.floor(Math.random()*n);
 
-// 1~n까지의 랜덤 수
+// 1~n까지의 랜덤수
 // Math.ceil(Math.random()*n)
 
-// 0~(n-1)까지의 랜덤 수
+// 0~n-1까지의 랜덤수
 // Math.floor(Math.random()*n)
 
-// 1. 기존 데이터에 새로운 속성 추가
+// 1. 기존 데이터에 새로운 속성추가
 guData.forEach((v)=>{
     v["소재"] = "천연가죽(소), 면100%";
     v["색상"] = "BLACK/TRUE WHITE";
@@ -30,4 +31,9 @@ guData.forEach((v)=>{
 
     // console.log(rdmNum(countryNum));
 })
+
+
+
+
+
 
