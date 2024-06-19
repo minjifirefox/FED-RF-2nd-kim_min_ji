89f7820e1ccbcbc3d7a4432872dcacbbb04e26d5
zoe <alswl3125@naver.com>
2024-06-19 16:48:09 +0900

    2024.06.19

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/FashionIntro.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/FashionIntro.jsx
@@ -1,20 +1,26 @@
-// 패션 인트로 컴포넌트 ///
-import React from "react";
+// 패션 인트로 컴포넌트 ////
+import React, { useContext } from "react";
+
+// 컨텍스트 API 불러오기
+import { pCon } from "./pCon";
 
 // 데이터 불러오기
 import { fsData } from "../../js/data/fashion_intro";
 
-// CSS 불러오기
+// CSS불러오기
 import "../../css/fashion_intro.scss";
 
 function FashionIntro({ catName, subCat, opt }) {
-  // 1. catName - 카테고리명
+  // 1. catName - 카테고리명 (men/women/style)
   // 2. subCat - 서브 카테고리명
-  // (서브가 아닌 경우 subCat의 값은 "etc"임!)
-  // 3. opt - 방향 옵션(역방향은 true / 정방향은 false)
-  // (역방향은 flex-direction: row-reverse; 적용!)
+  //  (서브가 아닌경우 subCat의 값은 "etc"임!)
+  // 3. opt - 방향옵션(역방향은 true / 정방향은 false)
+  //  (역방향은 flex-direction: row-reverse 적용!)
 
-  // 선택 데이터 변수 할당
+  // 컨텍스트 API사용하기
+  const myCon = useContext(pCon);
+
+  // 선택 데이터 변수할당
   const selData = fsData[catName];
 
   return (
@@ -34,31 +40,38 @@ function FashionIntro({ catName, subCat, opt }) {
         <li className="txtc">
           <h2 className={catName == "style" ? "tm" : ""}>
             {/* (참고) 데이터에 태그가 있어서 이를 html로 넣으려면
-            dangerouslySetInnerHTML={{__html:데이터}}
+            dangerouslySetInnerHTML={{__html:데이터}} 
             속성을 사용한다! */}
-            <a href="#">
-              {selData.tit[0][0]} <br />
+            <a href="#" onClick={(e)=>{
+              e.preventDefault();
+              myCon.setPgName(catName);
+            }}>
+              {selData.tit[0][0]}
+              <br />
               {selData.tit[0][1]}
             </a>
           </h2>
           {
-            // 스타일인 경우 글자 박스 하나 더 출력됨
+            // 스타일인 경우 글자박스 하나더 출력됨
             catName == "style" && (
               <h2 className="tw">
                 <a href="#">
-                  {selData.tit[1][0]} <br />
+                  {selData.tit[1][0]}
+                  <br />
                   {selData.tit[1][1]}
                 </a>
               </h2>
             )
           }
         </li>
-        {/* 3. 세번째 이미지 박스 : 스타일만! */}
+        {/* 3. 세번째 이미지박스 : 스타일만! */}
         {
-          // 스타일인 경우 li 이미지 박스 생성
+          // 스타일인 경우 li 이미지박스 생성
           catName == "style" && (
             <li className="imgc">
-              <img src={selData.isrc[1]} alt={selData.ialt[1]} />
+              <img 
+                src={selData.isrc[1]} 
+                alt={selData.ialt[1]} />
             </li>
           )
         }
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/pages/MainCont.jsx
@@ -1,37 +1,47 @@
 // 메인페이지 컨텐츠 컴포넌트 - MainCont.jsx
 import React, { useEffect, useLayoutEffect } from "react";
+
+// 모듈 불러오기
 import Banner from "../modules/Banner";
-// 자동 휠 함수 불러오기
-import * as wFn from "../../js/func/auto_wheel";
 import FashionIntro from "../modules/FashionIntro";
 
+// 제이쿼리
+import $ from "jquery";
+
+// 자동휠 함수 불러오기
+import * as wFn from "../../js/func/auto_wheel";
+
 function MainCont(props) {
-  // 화면 랜더링 실행 구역 //////////////////
+  // 화면 랜더링 실행구역 ///////
   useEffect(() => {
     console.log("useEffect");
 
     // document / body / window 이 세가지는
     // 이벤트를 등록하고 삭제할 수 있도록
-    // 1. 자동 휠 함수 이벤트 연결하기
+    // 1. 자동 휠함수 이벤트 연결하기
     window.addEventListener("wheel", wFn.wheelFn);
 
-    // 2. 메뉴 + 인디케이터 이벤트 기능 설정 함수 호출
+    // 2. 메뉴+인디케이터 이벤트 기능설정함수 호출
     wFn.evtFn();
 
-    // 3. 등장요소 CSS 초기화 함수 호출
+    // 3. 등장요소 CSS초기화 함수호출
     wFn.initSet();
 
-    // 컴포넌트 소멸시 이벤트 삭제하기
+    // 컴포넌트 소멸시 이벤트삭제하기
     return () => {
-      console.log("메인 소멸!");
+      console.log("메인소멸!");
+      // 1. 자동 휠함수 이벤트 삭제하기
+      window.removeEventListener("wheel", wFn.wheelFn);
+      // 2. 상단이동 이벤트 제거하기
+      // 제이쿼리 이벤트 제거는 off(이벤트명) 메서드
+      $("#logo a").off("click");
     };
   }, []);
-
   // useLayoutEffect(()=>{
   //   console.log("useLayoutEffect");
   // },[]);
 
-  // 코드 리턴 구역 /////////////////
+  ////// 코드 리턴구역 ///////////
   return (
     <>
       {/* 1. 배너 컴포넌트 */}
@@ -42,7 +52,6 @@ function MainCont(props) {
       >
         <Banner />
       </section>
-
       {/* 2. 남성패션 영역 */}
       <section className="page">
         <FashionIntro catName="men" subCat="etc" opt={false} />
