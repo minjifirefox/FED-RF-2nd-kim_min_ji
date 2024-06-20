71b54fd6a5d862b2e81691516f6a96ca11762f3c
zoe <alswl3125@naver.com>
2024-06-20 18:12:02 +0900

    2024.06.20

--- 004.React학습/02.react-ex/js/05.리액트변수.jsx
+++ 004.React학습/02.react-ex/js/05.리액트변수.jsx
@@ -1,7 +1,7 @@
 // 05.리액트변수 : useRef
 
 /******************************************************** 
-☑️ useRef
+☑️ useRef : 리액트 후크 참조 변수
 1. 특정 DOM 선택할때 주로 쓰이며 
 .current 프로퍼티로 전달된 인자로 
 초기화된 변경 가능한 ref 객체를 반환한다. 
@@ -9,6 +9,10 @@
 -> 예) <참조할요소/컴포넌트 ref="useRef사용변수명" .../>
 -> ref속성에 값으로 설정된 useRef변수를 써준다!
 이변수에 담긴 요소를 사용할 수 있다! -> 변수명.current
+
+-> current를 써야하는 이유는 useRef 컴포넌트 객체에서 이 변수를
+관리해주고 값을 유지 업데이트 하는데 사용하는 속성으로
+current를 사용하도록 지정함!
 
 2. 그밖에 컴포넌트가 리랜더링 되어도 변경되어서는
 안되는 변수의 값이 있다면 useRef를 사용하여 일정값을 유지함!
@@ -18,48 +22,78 @@
 const myRef = useRef(null);
 ********************************************************/
 
+// [4] 컴포넌트 외부 일반 변수
+// (1) 전역변수역할을 하지만 컴포넌트가 여러개 사용될 때
+// 이 변수를 업데이트 하면 여러가지 프로그램이 꼬인다!
+// 그러므로 절대! 절대! 절대! 변경되는 변수를 바깥 쪽에
+// 쓰지 않는다! -> 리액트 철칙!!!
+// (2) 단, 변경되지 않는 데이터 const 변수나 공유 함수 등은
+// 컴포넌트 바깥에 코딩할 수 있다!
+let what = "난뭐임?";
+
 // 메인 컴포넌트
 function MainComp() {
   console.log("컴포넌트 랜더링");
-  // 이름 상태관리 변수
+  // [1] 이름 상태관리 변수 :
+  // (1) 변경된 값을 컴포넌트 소멸 전까지 유지됨
+  // (2) 변수값 변경시 리랜더링 됨
+  // (3) 값 변경시 반영은 useEffect 구역에서 확인됨!
+  // -> 의미: 실제 돔에 반영할 때 변경 값이 업데이트 되는 시점임!
   const [stsName, setStsName] = React.useState("공유");
+  
+  // [2] 컴포넌트 내부 일반 변수
+  // -> 컴포넌트가 리랜더링 될 때 다시 초기화 함!
   let varName = "김수현";
-
+  
+  // [3] 리액트 참조 변수
+  // (1) 변경된 값을 컴포넌트 소멸 전까지 유지됨
+  // (2) 변수값 변경시 리랜더링 되지 않음!
   const refName = React.useRef("송새벽");
 
+  // 컴포넌트 화면 랜더링 실행 구역 //////////////////
   React.useEffect(() => {
     let ele = document.querySelectorAll(".name");
     ele[1].innerText = varName;
     ele[2].innerText = refName.current;
+
+    console.log("useState랜더링:", stsName)
   });
 
   // 이름바꾸기 함수
-  const changeName = React.useCallback(
+  const changeName = 
     (e) => {
+      // 1. 대상 버튼 텍스트 읽기
       let txt = e.target.innerText;
+      // 2. 앞에 형제요소인 input의 입력값 읽기
       let val = e.target.previousElementSibling.value;
+      // 3. 버튼별 분기 /////////////
       switch (txt) {
+        // (1) 상태 변수 업데이트
         case "이름바꿔 : useState":
-          console.log("변경전:", stsName);
+          console.log("useState변경전:", stsName);
           setStsName(val);
-          console.log("변경후:", stsName);
+          console.log("useState변경후:", stsName);
           break;
+        // (2) 컴포넌트 내부 일반 변수 업데이트
         case "이름바꿔 : let":
-          console.log("변경전:", varName);
+          console.log("let변경전:", varName);
           varName = val;
-          console.log("변경후:", varName);
-
+          console.log("let변경후:", varName);
           break;
+        // (3) 참조변수 업데이트
         case "이름바꿔 : useRef":
-          console.log("변경전:", refName.current);
+          console.log("useRef변경전:", refName.current);
           refName.current = val;
-          console.log("변경후:", refName.current);
-
+          console.log("useRef변경후:", refName.current);
+          break;
+        // (4) 외부변수 업데이트
+        case "이름바꿔 : outside":
+          console.log("outside변경전:", what);
+          what = val;
+          console.log("outside변경후:", what);
           break;
       }
-    },
-    [stsName]
-  );
+    };
 
   // 코드리턴 /////////////////
   return (
@@ -70,17 +104,22 @@ function MainComp() {
           <h1>
             1. useState 출력
             <br />
-            나는 <span className="name">{stsName}</span> 좋아해!
+            <b>나는 <span className="name">{stsName}</span> 좋아해!</b>
           </h1>
           <h1>
             2. let 출력
             <br />
-            나는 <span className="name">{varName}</span> 좋아해!
+            <b>나는 <span className="name">{varName}</span> 좋아해!</b>
           </h1>
           <h1>
             3. useRef 출력
             <br />
-            나는 <span className="name">{refName.current}</span> 좋아해!
+            <b>나는 <span className="name">{refName.current}</span> 좋아해!</b>
+          </h1>
+          <h1>
+            4. 바깥 let 출력
+            <br />
+            <b>나는 <span className="name">{what}</span> 좋아해!</b>
           </h1>
         </div>
         <div>
@@ -93,6 +132,9 @@ function MainComp() {
           <h2>3. useRef 이름변경</h2>
           <input type="text" className="ip-name3" /> &nbsp;
           <button onClick={changeName}>이름바꿔 : useRef</button>
+          <h2>4. 바깥let 이름변경</h2>
+          <input type="text" className="ip-name3" /> &nbsp;
+          <button onClick={changeName}>이름바꿔 : outside</button>
         </div>
       </div>
     </React.Fragment>
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Searching.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Searching.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 // 폰트어썸
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-// CSS 불러오기
+// CSS불러오기
 import "../../css/searching.scss";
 
 // 데이터 불러오기 : 캐릭터 리스트 데이터
@@ -18,31 +18,69 @@ function Searching({ kword }) {
   console.log("kword:", kword);
   console.log("data:", catListData);
 
+  // 키워드에 따라 검색결과가 달라지므로
+  // 핵심 데이터인 검색어를 상태관리변수로 만든다!!!
+  // ((상태관리변수)) //////////////
+  // [1] 검색어 상태관리변수
+  const [kw, setKw] = useState(kword);
+  // 초기값으로 전달받은 검색어 변수를 넣어준다!
+  // [2] 정렬기준 상태관리변수
+  const [sort, setSort] = useState("asc");
+  // 값: 오름차순 - asc / 내림차순 - desc
+  const [chk, setChk] = useState([true, true, true]);
+  console.log("체크값:",chk);
+
   // 검색어가 있는 데이터 필터하기
-  const newList = catListData.filter((v) => {
-    // 속성 중 캐릭터 이름 중 검색(v.cname)
-    // 검색어는 모두 영어일 경우 소문자 처리함
+  // filter()는 검색결과가 항상 배열로 나옴!
+  let newList = catListData.filter((v) => {
+    // 속성중 캐릭터 이름 중 검색(v.cname)
+    // 검색어는 모두 영어일 경우 소문자처리함
     let newVal = v.cname.toLocaleLowerCase();
-    // 전달받은 키워드도 소문자 처리
-    let key = kword.toLocaleLowerCase();
+    // 전달받은 키워드도 소문자처리
+    // ((중요!!!)) 상태변수인 kw로 대체한다!!!
+    let key = kw.toLocaleLowerCase();
     // 문자열이 있는 값만 배열로 재수집!
-    if (newVal.indexOf(kword) !== -1) return true;
-    // 문자열.indexOf(문자) 문자열 위치 번호 리턴 함
+    if (newVal.indexOf(key) !== -1) return true;
+    // 문자열.indexOf(문자) 문자열위치번호 리턴함
     // 그런데 결과가 없으면 -1을 리턴함!
-    // 그래서 -1이 아닐 경우 true를 리턴하면
+    // 그래서 -1이 아닐경우 true를 리턴하면
     // filter에서 변수에 저장할 배열로 수집된다!
-  }); //////////////////// filter //////////////////////
+  }); //////////////// filter ///////////////////
+
+  // [ 결과내 재검색 : 데이터 항목중 alignment값으로 검색함! ]
+  newList = newList.filter((v) => {
+    if(
+      chk[0]?v.alignment == "hero":false ||
+      chk[1]?v.alignment == "comp":false ||
+      chk[2]?v.alignment == "villain":false
+    
+    ) return true;
+  }); //////////////// filter ///////////////////
+
+  // [ 정렬기능 추가하기 ] /////////
+  // (1) 오름차순일 경우
+  if (sort == "asc") {
+    newList.sort((a, b) =>
+      a.cname > b.cname ? 1 : a.cname < b.cname ? -1 : 0
+    );
+  } /// if ///////////////////////
+  // (2) 내림차순일 경우
+  else if (sort == "desc") {
+    newList.sort((a, b) =>
+      a.cname > b.cname ? -1 : a.cname < b.cname ? 1 : 0
+    );
+  } /// else if ///////////////////
 
   console.log("newList:", newList);
-
   /* 
         변수 = 배열.filter(v=>{
-            if(v.indexOf(검색어)!=-1)return true
+            if(v.속성명.indexOf(검색어)!=-1) return true
         })
 
         -> 결과는 검색어가 있는 경우 변수에 모아서 담아준다!
-        -> 결과값도 배열, 결과가 없어도 빈 배열!
+        -> 결과값도 배열, 결과가 없어도 빈배열!
     */
+
   // 코드 리턴구역 ////////////////////////
   return (
     <>
@@ -64,6 +102,19 @@ function Searching({ kword }) {
               type="text"
               placeholder="Filter by Keyword"
               defaultValue={kword}
+              // 엔터키를 눌렀을때 검색실행!
+              // 검색어 상태변수만 업데이트하면 끝!!!
+              // -> setKw(검색어)
+              onKeyUp={(e) => {
+                if (e.key == "Enter") {
+                  // 검색어 상태값 변경
+                  setKw(e.target.value);
+                  // 처음 검색시 정렬은 기본정렬 오름차순(asc)
+                  setSort("asc");
+                  // 정렬선택박스 선택값변경(DOM에서 보이기변경)
+                  document.querySelector("#sel").value = "asc";
+                } /// if ///
+              }}
             />
           </div>
           {/* 1-2. 체크박스구역 */}
@@ -80,21 +131,53 @@ function Searching({ kword }) {
                   <li>
                     Heroes
                     {/* 숨긴 체크박스 */}
-                    <input type="checkbox" id="hero" className="chkhdn" />
+                    <input
+                      type="checkbox"
+                      id="hero"
+                      className="chkhdn"
+                      checked={chk[0]}
+                      // 체크변경시 change이벤트발생
+                      onChange={(e) => {
+                        // 체크박스의 checked속성은
+                        // 체크시 true, 불체크시 false리턴
+                        console.log(e.target.checked);
+                        setChk([e.target.checked,true,true]);
+                      }}
+                    />
                     {/* 디자인노출 라벨 */}
                     <label htmlFor="hero" className="chklb"></label>
                   </li>
                   <li>
                     It's Complicated
                     {/* 숨긴 체크박스 */}
-                    <input type="checkbox" id="comp" className="chkhdn" />
+                    <input type="checkbox" id="comp" className="chkhdn" 
+                    
+                    checked={chk[1]}
+                    // 체크변경시 change이벤트발생
+                    onChange={(e) => {
+                      // 체크박스의 checked속성은
+                      // 체크시 true, 불체크시 false리턴
+                      console.log(e.target.checked);
+                      setChk([true,e.target.checked,true]);
+                    }}
+                    />
                     {/* 디자인노출 라벨 */}
                     <label htmlFor="comp" className="chklb"></label>
                   </li>
                   <li>
                     Villains
                     {/* 숨긴 체크박스 */}
-                    <input type="checkbox" id="villain" className="chkhdn" />
+                    <input type="checkbox" id="villain" className="chkhdn" 
+                    
+                    checked={chk[2]}
+                    // 체크변경시 change이벤트발생
+                    onChange={(e) => {
+                      // 체크박스의 checked속성은
+                      // 체크시 true, 불체크시 false리턴
+                      console.log(e.target.checked);
+                      setChk([true,true,e.target.checked]);
+                    }}
+                    />
                     {/* 디자인노출 라벨 */}
                     <label htmlFor="villain" className="chklb"></label>
                   </li>
@@ -109,9 +192,19 @@ function Searching({ kword }) {
           <h2 className="restit">BROWSE CHARACTERS</h2>
           {/* 2-2. 정렬선택박스 */}
           <aside className="sortbx">
-            <select name="sel" id="sel" className="sel">
-              <option value="0">A-Z</option>
-              <option value="1">Z-A</option>
+            <select
+              name="sel"
+              id="sel"
+              className="sel"
+              // 값을 변경할때 이벤트발생
+              onChange={(e) => {
+                console.log(e.target.value);
+                // 정렬기준 상태변수 업데이트
+                setSort(e.target.value);
+              }}
+            >
+              <option value="asc">A-Z</option>
+              <option value="desc">Z-A</option>
             </select>
           </aside>
           {/* 2-3. 캐릭터 리스트 컴포넌트 : 
