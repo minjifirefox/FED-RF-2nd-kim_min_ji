08dc4dc4b2e910eb1ae072d8d21600ec532f4233
zoe <alswl3125@naver.com>
2024-07-04 17:43:49 +0900

    2024.07.04

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/SinSang.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/SinSang.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef } from "react";
 
 // 제이쿼리 불러오기
 import $ from "jquery";
@@ -9,10 +9,14 @@ import { showInfo, removeInfo, flowList } from "../../js/func/sinsang_fn";
 // 신상 데이터 불러오기 /////
 import { sinsangData } from "../../js/data/sinsang";
 import { useLayoutEffect } from "react";
+import { pCon } from "./pCon";
 
 function SinSang({ cat, chgItemFn }) {
   // cat - 카테고리 분류명 (men/women/style)
   // chgItemFn - 선택상품정보 변경 부모함수
+
+  // 전역 컨텍스트 사용하기
+  const myCon = useContext(pCon);
 
   // 신상품 리스트 이동함수 사용변수 ///
   // 위치값변수(left값) -> 리랜더링시 기존값을 유지하도록
@@ -53,7 +57,8 @@ function SinSang({ cat, chgItemFn }) {
             }}
           >
             <img
-              src={"./images/goods/" + cat + "/m" + (x + 1) + ".png"}
+              src={process.env.PUBLIC_URL+
+                "/images/goods/" + cat + "/m" + (x + 1) + ".png"}
               alt="신상품"
             />
           </a>
@@ -69,7 +74,10 @@ function SinSang({ cat, chgItemFn }) {
     <>
       <h2 className="c1tit">
         NEW MEN'S ARRIVAL
-        <button>전체리스트</button>
+        <button 
+        onClick={()=>myCon.setPgName
+        ("item-list")}
+        >전체리스트</button>
       </h2>
       <div
         className="flowbx"
